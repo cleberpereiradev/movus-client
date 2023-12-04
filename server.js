d7dab57399eb2af8161bea74839f0ac5ab2dec87
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-12-04 17:36:30 -0300

    prepare deploy in Heroku

--- server.js
+++ server.js
@@ -14,4 +14,7 @@ app.get('/*', (req, res) => {
 });
 
 
-app.listen(process.env.PORT());
+const PORT = process.env.PORT || 3000; // Use a porta fornecida pelo Heroku ou a porta 3000 por padrão
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
