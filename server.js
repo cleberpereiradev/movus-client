ad026d1f889a59d26b85eb84437bdad3612abc03
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-12-04 17:44:50 -0300

    resolving heroku deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const outputPath = `${__dirname}/dist/${appName}`;
 app.use(express.static(outputPath));
 
 app.get('/*', (req, res) => {
-    res.sendFile(`${outputPath}/index.html`);
+    res.sendFile(`${outputPath}`);
 });
 
 
