77cc41606c6d06a2a00e00b13be180dacad20ccc
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-12-04 17:53:23 -0300

    resolving heroku deploy

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-
-
+const path = require('path');
 const app = express();
 
-const appName = 'movus-app';
-
-const outputPath = `${__dirname}/dist/${appName}`;
-
+// Configuração para servir arquivos estáticos do Angular
 app.use(express.static(path.join(__dirname, 'dist/movus-app')));
 
-const PORT = process.env.PORT || 3000; // Use a porta fornecida pelo Heroku ou a porta 3000 por padrão
+// Rota para lidar com todas as outras solicitações
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/movus-app/index.html'));
+});
+
+// Use process.env.PORT ou a porta 3000 por padrão
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
