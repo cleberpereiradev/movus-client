f2deb358b1df01e0afcadfa7b180da8c7c1363fc
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-12-04 17:46:56 -0300

    resolving heroku deploy

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const appName = 'movus-app';
 
 const outputPath = `${__dirname}/dist/${appName}`;
 
-app.use(express.static(outputPath));
-
-app.get('/*', (req, res) => {
-    res.sendFile(`${outputPath}`);
-});
-
+app.use(express.static(path.join(__dirname, 'dist/movus-app')));
 
 const PORT = process.env.PORT || 3000; // Use a porta fornecida pelo Heroku ou a porta 3000 por padrão
 app.listen(PORT, () => {
